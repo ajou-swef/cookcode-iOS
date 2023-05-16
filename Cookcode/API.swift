82b360f6e3244be70ad0cc8e506a8806e84cb13e
99Page <nou0ggid@gmail.com>
2023-05-16 14:40:17 +0900

    rename: 깃액션 에러 확인을 위한 디렉토리 이동

---
+++ Cookcode/API.swift
@@ -0,0 +1,12 @@
+//
+//  da.swift
+//  Cookcode
+//
+//  Created by 노우영 on 2023/05/16.
+//
+
+import Foundation
+
+let BASE_URL = "http://54.180.117.179:8080"
+let ACCESS_TOKEN_KEY = "accessToken"
+let REFRESH_TOKEN_KEY = "refreshToken"
