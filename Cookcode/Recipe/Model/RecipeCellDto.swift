a8b0c125b694f6900340dde903aed1228a763cb2
99Page <nou0ggid@gmail.com>
2023-06-11 14:50:04 +0900

    feat: 레시피에서 부족한 식재료 표시

--- Cookcode/Recipe/Model/RecipeCellDto.swift
+++ Cookcode/Recipe/Model/RecipeCellDto.swift
@@ -9,19 +9,18 @@ import Foundation
 
 struct RecipeCellDto: Decodable, Hashable, Mock {
     static func mock() -> RecipeCellDto {
-        RecipeCellDto(recipeID: Int.random(in: 0...10000), user: UserCellDto.mock(), title: "title", description: "description", ingredients: IngredientDTO.mocks(1), optionalIngredients: IngredientDTO.mocks(1), createdAt: "2023-12-12", updatedAt: "2023-12-14", thumbnail: "https://picsum.photos/800/200", likeCount: 1)
+        RecipeCellDto(recipeID: Int.random(in: 0...10000), user: UserCellDto.mock(), title: "title", description: "description", createdAt: "2023-12-12", updatedAt: "2023-12-14", thumbnail: "https://picsum.photos/800/200", likeCount: 1)
     }
     
     let recipeID: Int
      let user: UserCellDto
      let title, description: String
-     let ingredients, optionalIngredients: [IngredientDTO]
      let createdAt, updatedAt, thumbnail: String
     let likeCount: Int
 
      enum CodingKeys: String, CodingKey {
          case recipeID = "recipeId"
-         case user, title, description, ingredients, optionalIngredients, createdAt, updatedAt, thumbnail
+         case user, title, description, createdAt, updatedAt, thumbnail
          case likeCount
      }
 }
